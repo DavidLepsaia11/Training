be9a186eb8b1f73fa21174c75c43ac4c829bcb1c
Davit Lepsaia <dlepsaia@rsm.ge>
2021-07-14 10:55:34 +0400

    GridAdd-On

---
+++ SAPB1/SBOAddonProject1/SBOAddonProject1/Form1.b1f.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using SAPbouiCOM.Framework;
+using SAPbouiCOM;
+
+namespace SBOAddonProject1
+{
+    [FormAttribute("SBOAddonProject1.Form1", "Form1.b1f")]
+    class Form1 : UserFormBase
+    {
+        public Form1()
+        {
+        }
+        private FormController formController;
+        private SAPbouiCOM.Grid grid;
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+            this.Button = ((SAPbouiCOM.Button)(this.GetItem("Item_0").Specific));
+            this.Button.PressedAfter += new SAPbouiCOM._IButtonEvents_PressedAfterEventHandler(this.Button_PressedAfter);
+            this.grid = ((SAPbouiCOM.Grid)(this.GetItem("Item_1").Specific));
+            this.OnCustomInitialize();
+
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+           
+        }
+
+        private SAPbouiCOM.Button Button;
+
+        private void OnCustomInitialize()
+        {
+            formController = new FormController(UIAPIRawForm, UIConnection.Company);
+
+            GridSetup();
+        }
+        private void GridSetup()
+        {
+            grid.DataTable.Columns.Add("Currency", BoFieldsType.ft_AlphaNumeric);
+            grid.DataTable.Columns.Add("Account Currency Code", BoFieldsType.ft_AlphaNumeric);
+            grid.DataTable.Columns.Add("Account Number", BoFieldsType.ft_AlphaNumeric);
+            grid.DataTable.Columns.Add("Position", BoFieldsType.ft_Integer);
+            grid.DataTable.Columns.Add("Intermediary  Bank Code", BoFieldsType.ft_AlphaNumeric);
+            grid.DataTable.Columns.Add("Intermediary Bank Name", BoFieldsType.ft_AlphaNumeric);        
+            grid.AutoResizeColumns();
+        }
+
+        private void Button_PressedAfter(object sboObject, SBOItemEventArg pVal)
+        {
+            formController.GetDatasInList();
+            formController.AddData(formController.AccountNumber.Count);
+        }
+    }
+}
---
+++ SAPB1/SBOAddonProject1/SBOAddonProject1/FormController.cs
@@ -0,0 +1,109 @@
+﻿using SAPbouiCOM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SBOAddonProject1
+{
+    class FormController
+    {
+        public SAPbouiCOM.IForm Form { get; set; }
+        public SAPbobsCOM.Company Company { get; set; }
+        private SAPbobsCOM.Recordset rs { get; set; }
+        public SAPbouiCOM.Grid Grid { get; set; }
+
+        #region Lists
+        public List<string> Currency;
+        public List<string> AccountCurrencyCode;
+        public List<string> AccountNumber;
+        public List<int> Position;
+        public List<string> IntermediaryBankCode;
+        public List<string> IntermediaryBankName;
+        #endregion
+
+        public FormController(SAPbouiCOM.IForm form, SAPbobsCOM.Company company)
+        {
+            Form = form;
+            Company = company;
+             Grid = ((SAPbouiCOM.Grid)(Form.Items.Item("Item_1").Specific));
+            Currency = new List<string>();
+            AccountCurrencyCode = new List<string>();
+            AccountNumber = new List<string>();
+            Position = new List<int>();
+            IntermediaryBankCode = new List<string>();
+           IntermediaryBankName = new List<string>();
+        }
+
+          public void GetDatasInList()
+        {
+            rs = (SAPbobsCOM.Recordset)Company.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
+            rs.DoQuery("SELECT TOP 24 * FROM DSC1");
+            Random r = new Random();
+            int  number;
+            string tempStr="";
+            string CurrencyCode = "";
+            while (!rs.EoF)
+            {
+                tempStr= rs.Fields.Item("Account").Value.ToString();
+                CurrencyCode = (tempStr[tempStr.Length - 3] + tempStr[tempStr.Length - 2] + tempStr[tempStr.Length - 1]).ToString();
+
+                AccountNumber.Add(tempStr);
+                IntermediaryBankCode.Add(rs.Fields.Item("BankCode").Value.ToString());
+                IntermediaryBankName.Add(rs.Fields.Item("AcctName").Value.ToString());
+                AccountCurrencyCode.Add(CurrencyCode);
+
+                number = r.Next(0, 100);
+                Position.Add(number);
+
+                switch (CurrencyCode)
+                {
+                    case "GEL":
+                        {
+                            Currency.Add("Gela");
+                            break;
+                        }
+                    case "EUR":
+                        {
+                            Currency.Add("EURO");
+                            break;
+                        }
+                    case "USD":
+                        {
+                            Currency.Add("American Dollar");
+                            break;
+                        }
+                    case "GBP":
+                        {
+                            Currency.Add("Pound Starling");
+                            break;
+                        }
+                    default:
+                        {
+                            Currency.Add("Other Currency");
+                            break;
+                        }
+                }
+
+                rs.MoveNext();
+            }
+        }
+          public void AddData(int Length )
+         {
+            Form.Freeze(true);
+            for (int i = 0; i < Length; i++)
+            {
+                Grid.DataTable.Rows.Add(1);
+                Grid.DataTable.Columns.Item("Currency").Cells.Item(i).Value = Currency[i];
+                Grid.DataTable.Columns.Item("Account Currency Code").Cells.Item(i).Value = AccountCurrencyCode[i];
+                Grid.DataTable.Columns.Item("Account Number").Cells.Item(i).Value = AccountNumber[i];
+                Grid.DataTable.Columns.Item("Position").Cells.Item(i).Value = Position[i];
+                Grid.DataTable.Columns.Item("Intermediary  Bank Code").Cells.Item(i).Value = IntermediaryBankCode[i];
+                Grid.DataTable.Columns.Item("Intermediary Bank Name").Cells.Item(i).Value = IntermediaryBankName[i];
+            }
+            Form.Freeze(false);
+        }
+    }
+}
